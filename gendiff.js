2caa1b0ffe15ef1549bd101324f023274c418d68
Bertie <bertyvuster@gmail.com>
2021-04-11 20:41:55 +0300

    some edits

--- gendiff.js
+++ gendiff.js
@@ -3,8 +3,6 @@
 import { Command } from 'commander/esm.mjs';
 
 const program = new Command();
-program.
-    option('-h, --help', 'output usage information')
-
 program.version('0.0.1', '-V, --version', 'output the version number');
+program.description('Compares two configuration files and shows a difference.');
 program.parse(process.argv);
