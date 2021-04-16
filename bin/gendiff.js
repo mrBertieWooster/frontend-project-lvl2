7b00c85bd67693605cf831b4f0a3be5db2a5c78d
Bertie <bertyvuster@gmail.com>
2021-04-16 18:46:38 +0300

    linter edits

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
-import { getDiff } from '../lib/index.js';
+import getDiff from '../lib/index.js';
 
 const program = new Command();
 program
-    .version('1.0.0', '-V, --version', 'output the version number')
-    .option('-f, --format [type]', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .description('Compares two configuration files and shows a difference.', {
-      filepath1: 'path to the first file',
-      filepath2: 'path to the second file'
-    })
-    .action((filepath1, filepath2, options, command) => {
-      console.log(getDiff(filepath1, filepath2));
-    });
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.', {
+    filepath1: 'path to the first file',
+    filepath2: 'path to the second file',
+  })
+  .action((filepath1, filepath2, options, command) => {
+    console.log(getDiff(filepath1, filepath2));
+  });
 
 program.parse(process.argv);
