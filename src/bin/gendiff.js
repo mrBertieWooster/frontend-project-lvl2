8e51937e753558bfad19bd0e309c628cedce77e6
bertie <bertyvuster@gmail.com>
2021-05-04 16:07:51 +0300

    gendiff updated

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,19 +1,14 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
-import { readFileSync } from 'fs';
 import getDiff from '../index.js';
-
-const packageJson = JSON.parse(readFileSync('../../package.json'));
-const version = packageJson.version || 0;
-const description = packageJson.description || '';
 
 const program = new Command();
 program
-  .version(version, '-V, --version', 'output the version number')
+  .version('0.3.18', '-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format [type]', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .description(description, {
+  .description('get difference between two files', {
     filepath1: 'path to the first file',
     filepath2: 'path to the second file',
   })
